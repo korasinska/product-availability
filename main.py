f88713e323126517efeab7c27a81a5b6a9b1c760
korasinska <klaudiaorasinska@gmail.com>
2025-07-28 10:28:44 +0200

    update

--- main.py
+++ main.py
@@ -9,52 +9,39 @@ PASSWORD = os.environ["FROM_EMAIL_PASSWORD"]
 product_url = "https://ubranesklep.pl/produkt/7854/spodnie-nico-kokos"
 size = "S"
 
-response = requests.get(product_url)
-html = response.text
+try:
+    response = requests.get(product_url)
+    response.raise_for_status()
+    html = response.text
+except Exception as e:
+    print(f"Błąd przy pobieraniu strony: {e}")
 
 soup = BeautifulSoup(html, "html.parser")
-option = soup.select_one("select#inventory_id > option")
+options = soup.select("select#inventory_id > option")
+sizes = [opt.text.strip() for opt in options]
 title = soup.select_one("article.article-product > h1")
 
-msg = EmailMessage()
-msg["From"] = MY_EMAIL
-msg["To"] = "klaudiaorasinska@gmail.com"
-
-def send_mail_product_not_available(msg):
-    msg["Subject"]= "Produkt został usunięty!"
-    msg.set_content(f"Produkt na który czekasz {product_url} został usunięty ze strony :(")
+def send_mail(subject, content):
+    msg = EmailMessage()
+    msg["From"] = MY_EMAIL
+    msg["To"] = "klaudiaorasinska@gmail.com"
+    msg["Subject"]= subject
+    msg.set_content(content)
     with smtplib.SMTP("smtp.gmail.com", 587) as connection:
         connection.starttls()
         connection.login(user=MY_EMAIL, password=PASSWORD)
         connection.send_message(msg=msg)
-
-def send_mail_product_available(msg):
-    msg["Subject"]  = "Produkt znów dostępny!"
-    msg.set_content(f"Produkt na który czekasz {title.text} w rozmiarze {size} znów jest dostępny w sprzedaży!<3\nLINK: {product_url}")
-    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
-        connection.starttls()
-        connection.login(user=MY_EMAIL, password=PASSWORD)
-        connection.send_message(msg=msg)
-
-def send_mail_size_not_available(msg):
-    msg["Subject"] = "Produkt nie jest dostępny w tym rozmiarze!"
-    msg.set_content(f"Produkt na który czekasz {title.text} nie jest dostępny w rozmiarze {size}.\nLINK: {product_url}")
-    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
-        connection.starttls()
-        connection.login(user=MY_EMAIL, password=PASSWORD)
-        connection.send_message(msg=msg)
-
 
 if title is not None:
-    if option is not None:
-        if size not in option.text:
-            send_mail_product_available(msg)
+    if options is not None:
+        if size not in sizes:
+            send_mail("Produkt znów dostępny!", f"Produkt na który czekasz {title.text} w rozmiarze {size} znów jest dostępny w sprzedaży!<3\nLINK: {product_url}")
         else:
-            send_mail_size_not_available(msg)
+            send_mail("Produkt nie jest dostępny w tym rozmiarze!", f"Produkt na który czekasz {title.text} nie jest dostępny w rozmiarze {size}.\nLINK: {product_url}")
     else:
-         send_mail_product_available(msg)
+         send_mail("Produkt znów dostępny!", f"Produkt na który czekasz {title.text} w rozmiarze {size} znów jest dostępny w sprzedaży!<3\nLINK: {product_url}")
 else:
-    send_mail_product_not_available(msg)
+    send_mail("Produkt został usunięty!", f"Produkt na który czekasz {product_url} został usunięty ze strony :(")
 
 
 
